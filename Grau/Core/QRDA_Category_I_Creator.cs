6a3dc466d4817ff85eba638c86ecc626b5967722
grosales <grosales@aileronconsulting.com>
2013-10-04 10:57:23 -0400

    Added company link

--- Grau/Core/QRDA_Category_I_Creator.cs
+++ Grau/Core/QRDA_Category_I_Creator.cs
@@ -1,5 +1,6 @@
 ﻿/*
-   Copyright 2013 Aileron Consulting LLC
+   Copyright 2013 Aileron Consulting LLC 
+   http://www.aileronconsulting.com
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
